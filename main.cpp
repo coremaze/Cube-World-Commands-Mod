daea14395aefdaaa510a7f40274237b45f9af6cf
ChrisMiuchiz <chris000005@gmail.com>
2019-09-24 19:01:35 -0400

    Fix bugs and add /name

--- main.cpp
+++ main.cpp
@@ -2,13 +2,14 @@
 #include "cwmods/cwmods.h"
 #include <wchar.h>
 #include <stdio.h>
+#include <string.h>
 
 
-float GetMilliseconds(int hour, int minute){
+float GetMilliseconds(int hour, int minute) {
     return (hour * 60 * 60 * 1000) + (minute * 60 * 1000);
 }
 
-void CommandsModMessage(wchar_t message[]){
+void CommandsModMessage(wchar_t message[]) {
     cube::Game* game = cube::GetGame();
     game->chat_widget->PrintMessage(L"[");
     game->chat_widget->PrintMessage(L"CommandsMod", 255, 140, 0);
@@ -35,35 +36,55 @@ EXPORT int HandleChat(wchar_t* msg) {
 
     int targetHour, targetMinute;
 
-    if ( swscanf(msg, L"/settime %d:%d", &targetHour, &targetMinute) == 2){
+    if ( swscanf(msg, L"/settime %d:%d", &targetHour, &targetMinute) == 2) {
         printf("%ld:%ld\n", targetHour, targetMinute);
         float targetMS = GetMilliseconds(targetHour, targetMinute);
 
         float maxTime = GetMilliseconds(24, 0);
         float minTime = GetMilliseconds(0, 0);
 
-        if (targetMS > maxTime) targetMS = maxTime;
-        else if (targetMS < minTime) targetMS = minTime;
+        if (targetMS > maxTime)
+            targetMS = maxTime;
+        else if (targetMS < minTime)
+            targetMS = minTime;
 
         world->time = targetMS;
 
         swprintf(response, L"Changing time to %d:%02d.\n", targetHour % 24, targetMinute % 60);
         CommandsModMessage(response);
         return 1;
-    }
-    else if (!wcscmp(msg, L"/coords")){
+
+    } else if (!wcscmp(msg, L"/coords")) {
         cube::Creature* player = game->GetPlayer();
         swprintf(response, L"World coordinates:\nX: %lld\nY: %lld\nZ: %lld\n", player->position.x, player->position.y, player->position.z);
         CommandsModMessage(response);
         return 1;
-    }
-    else if ( swscanf(msg, L"/tp %d %d", &targetx, &targety) == 2){
-            cube::Creature* player = game->GetPlayer();
-            player->position.x = MapCoordToDots(targetx);
-            player->position.y = MapCoordToDots(targety);
-            swprintf(response, L"Teleporting.\n");
-            CommandsModMessage(response);
-            return 1;
+
+    } else if ( swscanf(msg, L"/tp %d %d", &targetx, &targety) == 2) {
+        cube::Creature* player = game->GetPlayer();
+        player->position.x = MapCoordToDots(targetx);
+        player->position.y = MapCoordToDots(targety);
+        CommandsModMessage(L"Teleporting.\n");
+        return 1;
+
+    } else if ( !wcsncmp(msg, L"/name ", 6) ) {
+        cube::Creature* player = game->GetPlayer();
+        wchar_t* wideName = &msg[6];
+        char newName[18];
+        memset(newName, 0, sizeof(newName));
+        int len = wcstombs(newName, wideName, 16);
+        if (len < 1) {
+            CommandsModMessage(L"Invalid name.\n");
         }
+        else if (len >= 16) {
+            CommandsModMessage(L"Name too long!\n");
+        }
+        else {
+            strcpy(player->name, newName);
+            swprintf(response, L"You are now known as %s.\n", player->name);
+            CommandsModMessage(response);
+        }
+        return 1;
+    }
     return 0;
 }
